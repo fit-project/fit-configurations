2824473ff823ec6601e114326828294b43f82ed6
Fabio Zito <zitelog@gmail.com>
2025-04-26 11:18:29 +0200

    add LogConfigTools

--- fit_configurations/utils.py
+++ fit_configurations/utils.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#######
+# -----
+# Copyright (c) 2023 FIT-Project
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# -----
+#######
+
 from fit_configurations.view.configuration import Configuration
 
 from fit_configurations.controller.tabs.language.language import (
