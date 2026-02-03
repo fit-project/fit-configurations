d48d84dcec520bd4eb9c45e05cbcc8e0e6b4051e
Fabio Zito <zitelog@gmail.com>
2026-02-03 15:54:48 +0100

    feat(general): set OS-specific user agent with version token
    remove DEFAULT_USER_AGENT from translations and build UA in code per platform, appending FreezingInternetTool/<version> when available

--- fit_configurations/model/tabs/general/general.py
+++ fit_configurations/model/tabs/general/general.py
@@ -7,7 +7,7 @@
 # -----
 ######
 
-from fit_common.core import get_platform
+from fit_common.core import get_platform, get_version
 from sqlalchemy import Column, Integer, String
 
 from fit_configurations.model.tabs.tab import TabModel
@@ -29,10 +29,22 @@ class GeneralModel(TabModel):
             "win": "~/Documents/FIT",
             "other": "~/Documents/FIT",
         }
+        default_user_agent_by_os = {
+            "lin": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+            "macos": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15",
+            "win": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/6.5.0 Chrome/108.0.5359.220 Safari/537.36",
+            "other": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/6.5.0 Chrome/108.0.5359.220 Safari/537.36",
+        }
 
-        self.cases_folder_path = default_path_by_os[get_platform()]
+        platform = get_platform()
+        self.cases_folder_path = default_path_by_os[platform]
         self.home_page_url = "http://fit-project.org/"
-        self.user_agent = self.translations["DEFAULT_USER_AGENT"]
+        base_user_agent = default_user_agent_by_os[platform]
+        version = get_version()
+        if version:
+            self.user_agent = f"{base_user_agent} FreezingInternetTool/{version}"
+        else:
+            self.user_agent = base_user_agent
         self.language = "english"
 
         self.db.session.add(self)
