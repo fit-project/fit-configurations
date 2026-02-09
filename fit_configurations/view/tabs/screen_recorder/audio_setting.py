d9329370ce43810f8dd014413fe8d406ff855135
Fabio Zito <zitelog@gmail.com>
2026-02-09 18:04:29 +0100

    refactor(screen-recorder): apply centralized UI translation and adjust packaging excludes
    - use `translate_ui` in `AudioSetting` to initialize dialog translations centrally
    - rename `translations` to `__translations` and update all references for encapsulation
    - update Poetry config to exclude `fit_configurations/ui/**` from packaging

--- fit_configurations/view/tabs/screen_recorder/audio_setting.py
+++ fit_configurations/view/tabs/screen_recorder/audio_setting.py
@@ -9,6 +9,7 @@
 
 from fit_common.core import is_cmd
 from fit_common.gui.clickable_label import ClickableLabel as ClickableLabelView
+from fit_common.gui.ui_translation import translate_ui
 from PySide6 import QtCore, QtWidgets
 from PySide6.QtGui import QPixmap
 from PySide6.QtMultimedia import QMediaDevices
@@ -23,7 +24,7 @@ class AudioSetting(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.translations = load_translations()
+        self.__translations = load_translations()
 
         # HIDE STANDARD TITLE BAR
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
@@ -32,6 +33,8 @@ class AudioSetting(QtWidgets.QDialog):
         self.ui = Ui_audio_recording_checker_dialog()
         self.ui.setupUi(self)
 
+        translate_ui(self.__translations, self)
+
         __icon_green = QPixmap(":/icons/icons/green-mark.png").scaled(20, 20)
         __icon_red = QPixmap(":/icons/icons/red-mark.png").scaled(20, 20)
 
@@ -39,41 +42,43 @@ class AudioSetting(QtWidgets.QDialog):
 
         if self.is_installed_ffmpeg():
             self.ui.ffmpeg_installed_img.setPixmap(__icon_green)
-            self.ui.ffmpeg_installed_msg.setText(self.translations["FFMPEG_INSTALLED"])
+            self.ui.ffmpeg_installed_msg.setText(
+                self.__translations["FFMPEG_INSTALLED"]
+            )
         else:
             self.ui.ffmpeg_installed_img.setPixmap(__icon_red)
             self.ui.ffmpeg_installed_msg.setText(
-                self.translations["FFMPEG_NOT_INSTALLED"]
+                self.__translations["FFMPEG_NOT_INSTALLED"]
             )
 
         if self.get_vb_cable_virtual_audio_device() is not None:
             self.ui.vb_cable_installed_img.setPixmap(__icon_green)
             self.ui.vb_cable_installed_msg.setText(
-                self.translations["VB_CABLE_INSTALLED"]
+                self.__translations["VB_CABLE_INSTALLED"]
             )
             self.ui.vb_cable_box_first_output_audio_device.setVisible(True)
         else:
             self.ui.vb_cable_installed_img.setPixmap(__icon_red)
             self.ui.vb_cable_installed_msg.setText(
-                self.translations["VB_CABLE_INSTALLED"]
+                self.__translations["VB_CABLE_INSTALLED"]
             )
             self.ui.vb_cable_box_first_output_audio_device.setVisible(False)
 
         if self.is_vb_cable_first_ouput_audio_device():
             self.ui.vb_cable_first_output_audio_device_img.setPixmap(__icon_green)
             self.ui.vb_cable_first_output_audio_device_msg.setText(
-                self.translations["VB_CABLE_FIRST_OUPUT_AUDIO_DEVICE"]
+                self.__translations["VB_CABLE_FIRST_OUPUT_AUDIO_DEVICE"]
             )
         else:
             self.ui.vb_cable_first_output_audio_device_img.setPixmap(__icon_red)
             self.ui.vb_cable_first_output_audio_device_msg.setText(
-                self.translations["VB_CABLE_NOT_FIRST_OUPUT_AUDIO_DEVICE"]
+                self.__translations["VB_CABLE_NOT_FIRST_OUPUT_AUDIO_DEVICE"]
             )
 
         self.ui.guide_link.addWidget(
             ClickableLabelView(
-                self.translations["AUDIO_RECORDING_MANAGEMENT_GUIDE_URL"],
-                self.translations["AUDIO_RECORDING_MANAGEMENT_GUIDE"],
+                self.__translations["AUDIO_RECORDING_MANAGEMENT_GUIDE_URL"],
+                self.__translations["AUDIO_RECORDING_MANAGEMENT_GUIDE"],
             )
         )
 
